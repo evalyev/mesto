724472feaa8798d43e197fecb61b48564f9a2a3d
Evgeniy Bakaev <bak.evsha@yandex.ru>
2021-04-22 00:45:52 +0300

    fix: исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,32 @@
 let popup = document.querySelector('.popup');
-let popupForm = document.querySelector('.popup__container');
-let popupName = document.querySelector('.popup__name');
-let popupDescription = document.querySelector('.popup__description');
+let popupForm = document.querySelector('.popup__form');
+let popupName = document.querySelector('.popup__input-text_profile_name');
+let popupDescription = document.querySelector('.popup__input-text_profile_description');
 let popupClose = document.querySelector('.popup__close');
 
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let profileEditButton = document.querySelector('.profile__edit-button');
 
-function editButton() {
+function openPopup() {
   popup.classList.add('popup_opened');
   popupName.setAttribute('value', profileName.textContent);
   popupDescription.setAttribute('value', profileDescription.textContent);
 }
 
-profileEditButton.addEventListener('click', editButton);
-
-popupClose.addEventListener('click', function(){
+function closePopup() {
   popup.classList.remove('popup_opened');
-});
+}
+
+profileEditButton.addEventListener('click', openPopup);
+
+popupClose.addEventListener('click', closePopup);
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = popupName.value;
   profileDescription.textContent = popupDescription.value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
 
 popupForm.addEventListener('submit', formSubmitHandler); 
