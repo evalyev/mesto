4bdc0c38c8aa02f64e715549298a0d47f0879659
Evgeniy Bakaev <bak.evsha@yandex.ru>
2021-05-05 20:41:30 +0300

    feat: функционал добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,13 @@ let popupForm = document.querySelector('.popup__form');
 let popupName = document.querySelector('.popup__input-text_profile_name');
 let popupDescription = document.querySelector('.popup__input-text_profile_description');
 let popupClose = document.querySelector('.popup__close');
+const popupTitle = document.querySelector('.popup__title')
+const popupSubmit = document.querySelector('.popup__submit');
 
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let profileEditButton = document.querySelector('.profile__edit-button');
+const profileAddButton = document.querySelector('.profile__add-button');
 
 const elementContainer = document.querySelector('.elements');
 const cardTemplate = document.querySelector('#element');
@@ -38,7 +41,15 @@ const initialCards = [
   }
 ]; 
 
-profileEditButton.addEventListener('click', openPopup);
+profileAddButton.addEventListener('click', function() {
+  openPopup();
+  cardPopup();
+})
+
+profileEditButton.addEventListener('click', function() {
+  openPopup();
+  profilePopup();
+});
 
 popupClose.addEventListener('click', closePopup);
 
@@ -48,10 +59,27 @@ initialCards.forEach(item => {
   elementContainer.append(addCard(item.name, item.link));
 })
 
+
 function openPopup() {
   popup.classList.add('popup_opened');
-  popupName.setAttribute('value', profileName.textContent);
-  popupDescription.setAttribute('value', profileDescription.textContent);
+}
+
+function cardPopup() {
+  popupTitle.textContent = 'Новое место';
+  popupName.setAttribute('placeholder', 'Название');
+  popupDescription.setAttribute('placeholder', 'Ссылка на картинку');
+  popupName.value = '';
+  popupDescription.value = '';
+  popupSubmit.textContent = 'Создать';
+}
+
+function profilePopup() {
+  popupTitle.textContent = 'Редактировать профиль';
+  popupName.setAttribute('placeholder', 'Ваше имя');
+  popupDescription.setAttribute('placeholder', 'Описание');
+  popupName.value = profileName.textContent;
+  popupDescription.value = profileDescription.textContent;
+  popupSubmit.textContent = 'Сохранить';
 }
 
 function closePopup() {
@@ -60,8 +88,13 @@ function closePopup() {
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
+  if(popupTitle.textContent === 'Редактировать профиль') {
   profileName.textContent = popupName.value;
   profileDescription.textContent = popupDescription.value;
+  }
+  else if (popupTitle.textContent === 'Новое место') {
+    elementContainer.prepend(addCard(popupName.value, popupDescription.value));
+  }
   closePopup();
 }
 
